0c7c889658f78dca03aeaa538b3960c14ede8b8d
Ryan Batchelder <rb688@nau.edu>
2015-02-23 12:14:50 -0700

    Started adding a bubble object that we can create dynamically

---
+++ src/Bubble.hx
@@ -0,0 +1,15 @@
+import flash.textures.Texture;
+import flash.display.Image;
+import flash.geom.Rectangle;
+import starling.core.Starling;
+
+class Bubble extends Image
+{
+	function new(bubbleType:Int):Image
+	{
+		// TODO: Grab the type of bubble texture from an array and pass it into Super()
+		super();
+
+		return this;
+	}
+}
